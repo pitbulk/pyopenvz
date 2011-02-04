14c809e39609b7cdc5ffb7182a1463230e805508
mviera <manuel.viera.tirado@gmail.com>
2011-02-04 19:49:41 +0100

    pyopenvz setup.py file.

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+setup(
+    name='pyopenvz',
+    version='0.1',
+    url='http://github.com/mviera/pyopenvz',
+    license='LGPLv3',
+    description='A Python remote API RESTful for OpenVZ',
+    author='Manuel Viera <manuel.viera.tirado@gmail.es>, Sixto Martin <pitbulk@gmail.com>',
+    classifiers=[
+        'Development Status :: early stages',
+        'Intended Audience :: Developers',
+        'License :: EUPL',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    py_modules=['pyopenvz'],
+    install_requires=[
+        'distribute==0.6.14',
+    ],
+)
